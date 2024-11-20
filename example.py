7d24d1ba40ef98cd0bce45a6e46e70ce35b68dda
PriscillaEwusi <addopriscilla373@gmail.com>
2024-11-20 11:07:50 +0000

    initial commit

---
+++ example.py
@@ -0,0 +1,60 @@
+"""
+x = 5
+y = "John"
+print (type(x))
+print (type(y))
+"""
+
+"""days =["Monday", "Tuesday", "Wednesday"]
+x, y, z = days
+print (x)
+print (y)
+print (z)
+"""
+"""x = y = z = "Friday"
+print(x)
+print(y)
+print(z)"""
+
+"""
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x + y + z)
+"""
+
+# def f():
+#     s = "Priscilla"
+#     print ("inside function", s)
+    
+
+# s = "I love geeksforgeeks"
+# f()
+# print ("outside function", s)
+
+# def add_two_numbers(number_one, number_two):
+#       return number_one + number_two   
+
+# sum_with_return = add_two_numbers(5, 4)
+# print (sum_with_return)
+
+ # Calling the upper() method for the built-in str class
+start_text = "my sentence for example."
+print (str.upper(start_text))
+
+
+def number_to_the_power_of(number_one, number_two):
+        return number_one ** number_two
+
+print (number_to_the_power_of(5, 5))
+
+
+
+
+
+
